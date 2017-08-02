0bd6c3c30a39850fce51417e1fbf62135d0fc235
Anderson da Silva Macedo <dasilvamacedo.anderson@gmail.com>
2017-08-01 22:24:26 -0300

    Commit

---
+++ MountainStyleShop.ModelNH/Model/AvaliacaoProduto.cs
@@ -0,0 +1,54 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MountainStyleShop.ModelNH.Model
+{
+    public class AvaliacaoProduto
+    {
+        public virtual int Id { get; set; }
+
+        [Display(Name = "Texto da avaliação")]
+        [Required(ErrorMessage = "O Texto é Obrigatorio.")]
+        public virtual String Texto { get; set; }
+        
+        public virtual int NotaAvaliacao { get; set; }
+
+        public virtual Produto Produto { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+
+        public virtual DateTime Data { get; set; }
+    }
+
+    public class AvaliacaoProdutoMap : ClassMapping<AvaliacaoProduto>
+    {
+        public AvaliacaoProdutoMap()
+        {
+            Id<int>(x => x.Id, m =>
+            {
+                m.Generator(Generators.Identity);
+            });
+
+            Property<String>(x => x.Texto);
+            Property<int>(x => x.NotaAvaliacao);
+            Property<DateTime>(x => x.Data);
+
+            ManyToOne<Produto>(x => x.Produto, m =>
+            {
+                m.Column("IdProduto");
+            });
+
+            ManyToOne<Usuario>(x => x.Usuario, m =>
+            {
+                m.Column("IdUsuario");
+            });
+        }
+
+    }
+}
---
+++ MountainStyleShop.ModelNH/Repository/RepositoryBase.cs
@@ -0,0 +1,53 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MountainStyleShop.ModelNH.Repository
+{
+    public abstract class RepositoryBase<T> where T : class
+    {
+        protected ISession session = null;
+
+        public RepositoryBase(ISession session)
+        {
+            this.session = session;
+        }
+
+        public void Start()
+        {
+            this.session.BeginTransaction();
+        }
+
+        public bool Gravar(T entity)
+        {
+
+            var transacao = this.session.BeginTransaction();
+            this.session.SaveOrUpdate(entity);
+            transacao.Commit();
+            return true;
+        }
+
+        public bool Delete(T entity)
+        {
+            this.session.Clear();
+            var transacao = this.session.BeginTransaction();
+            this.session.Delete(entity);
+            transacao.Commit();
+            return true;
+        }
+
+        public IList<T> BuscaTodos()
+        {
+            return this.session.CreateCriteria<T>().List<T>();
+        }
+
+        public T BuscaPorId(int id)
+        {
+
+            return this.session.Get<T>(id);
+        }
+    }
+}
