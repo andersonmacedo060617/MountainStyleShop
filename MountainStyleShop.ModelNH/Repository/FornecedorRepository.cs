22812909f3b94ee17c7548cae9c39d92b5bea16e
Anderson da Silva Macedo <dasilvamacedo.anderson@gmail.com>
2017-08-21 00:25:46 -0300

    -Commit

---
+++ MountainStyleShop.ModelNH/Repository/FornecedorRepository.cs
@@ -0,0 +1,19 @@
+﻿using MountainStyleShop.ModelNH.Model;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MountainStyleShop.ModelNH.Repository
+{
+    public class FornecedorRepository : RepositoryBase<Fornecedor>
+    {
+        public FornecedorRepository(ISession session) : base(session)
+        {
+        }
+    }
+    {
+    }
+}
